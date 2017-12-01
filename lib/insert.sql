9acc3d8af8bc5a3e5de2be8992476f54eba51474
Eric McKinney <mckinney99@gmail.com>
2017-12-01 21:36:57 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO series (title) VALUES ("Mu"), ("Lu");
+
+INSERT INTO books (title, year) VALUES ("hello", 1997), ("hello", 1997), ("hello", 1997), ("hello", 1997), ("hello", 1997), ("hello", 1997);
+
+INSERT INTO characters (name, motto, species) VALUES ("Thor", "I love snuggles!", "not-human"), ("Thor", "I love snuggles!", "not-human"), ("Thor", "I love snuggles!", "not-human"), ("Thor", "I love snuggles!", "not-human"), ("Thor", "I love snuggles!", "not-human"), ("Thor", "I love snuggles!", "not-human"), ("Thor", "I love snuggles!", "not-human"), ("Thor", "I love snuggles!", "not-human");
+
+INSERT INTO subgenres (name) VALUES ("Harry"), ("Potter");
+
+INSERT INTO authors (name) VALUES ("JK. ROWLING"), ("JRR TOLKEN");
+
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1);
